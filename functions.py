bd9616160b863a95200d60082e549ed7c20b4b5e
Omar Vizcaino <omarvizcainoorozco@gmail.com>
2026-03-20 15:17:24 -0500

    new update

--- functions.py
+++ functions.py
@@ -1,33 +1,43 @@
+#we called since ramdom to can use the library and use the funcions from library
 import random
 
-def generate_damage(min, max):
+#this function let's us know the damage, we use 2 parameter and the always return in a number int
+# we import from library the function radom.randint.
+def generate_damage(min: int, max: int):
     damage = random.randint(min, max)
     return damage
 
-def show_state(name_hero, hp_hero, name_enemy, hp_enemy):
-    hp = {
-            name_hero: hp_hero,
-            name_enemy: hp_enemy
-    }
 
-    print() ############# falta
+#This function show us the name and the hp of the players
+def show_state(hp_hero, hp_enemy):
 
-    return hp
+    print(f"Life hero: {hp_hero}")
+    print(f"Life enemy: {hp_enemy}")
 
-def turn_player():
+#this functions show the turn between player called heroe 
+def turn_player(hp_hero, hp_enemy, potions):
     action = input("1. Attack\n2. Heal\n3. Special ability\nWhat do you want to do?: ").lower()
 
     if action == "attack" or action == "1":
-        damage = random.randint(10, 25)
+        damage = generate_damage(10, 25)
         print(f"Damage caused: {damage}")
-    
+        hp_enemy -= damage
+        hp_hero -= turn_enemy()
+        print(f"The enemy has attacked: {turn_enemy()}")
+
     elif action == "heal" or action == "2":
-        print("You have healed: +20 life")
+        if potions > 0:           
+            potions -= 1
+            hp_hero += 20
+            print("You have healed: +20 life")
+            print(f"stock's potions {potions}")
+        else:
+            print("You haven't stock")
     
     elif action == "special ability" or action == "3":
         probability = random.randint(0, 1)
 
-        if probability == 0:
+        if probability == 1:
             great_damage = random.randint(30, 50)
             print("The special ability was successfully executed!")
             print(f"Damage caused by the great damage: {great_damage}")
@@ -37,13 +47,19 @@ def turn_player():
     
     else:
         print("Invalid option")
+    
+    show_state(hp_hero,hp_enemy)
+    
+    return hp_hero, hp_enemy, potions
 
+#this functions show the turn between player called monster
 def turn_enemy():
     attack = random.randint(15, 20)
     return attack
 
+#this function  is to prove who's win
 def verify_winner(hp_hero, hp_enemy):
-    verify = True ########################### en duda
+    verify = True 
 
     if hp_hero == 0:
         print("You win!")
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import functions
 
-
-print(functions.generate_damage(10, 50))
-
-name_hero = "Omar"
+name_hero = input("What is your name?: ")
 hp_hero = 100
-name_enemy = "Mounstro"
+name_enemy = "Monster"
 hp_enemy = 120
+potions = 3
 
-print(functions.show_state(name_hero, hp_hero, name_enemy, hp_enemy))
+functions.show_state(hp_hero, hp_enemy)
 
-functions.turn_player()
-
-print("Damage caused by enemy: ",functions.turn_enemy())
-
-print(functions.verify_winner(20, 0))
+while functions.verify_winner(hp_hero, hp_enemy):
+    functions.turn_player(hp_hero, hp_enemy, potions)
