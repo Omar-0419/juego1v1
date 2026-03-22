fc9d4ba1b310d6425912cbd3fc08fa50dbc57f08
Omar Vizcaino <omarvizcainoorozco@gmail.com>
2026-03-21 20:12:15 -0500

    last update

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ potions = 3
 
 while functions.verify_winner(hp_hero, hp_enemy):
     hp_hero, hp_enemy, potions = functions.turn_player(hp_hero, hp_enemy, potions)
-    functions.show_state(hp_hero, hp_enemy)
+    functions.show_state(hp_hero = max(0, hp_hero), hp_enemy = max(0, hp_enemy))
