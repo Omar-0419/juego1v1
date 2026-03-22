37972f9c87edc196a92f7ce973971eacaa3b3d9e
Kerlys Domenech <122834334+Kerdomenech@users.noreply.github.com>
2026-03-21 20:25:45 -0500

    #

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+#
 import functions
 
+ # Initial Attributes 
 name_hero = input("What is your name?: ")
 hp_hero = 100
 name_enemy = "Monster"
 hp_enemy = 120
 potions = 3
 
+#Main Control Logic
+
+#Main loop ends when HP <= 0 
 while functions.verify_winner(hp_hero, hp_enemy):
+   #we import from funtions this varibles. we import Player's turn and Check if enemy or hero died to stop the turn and finished the game
     hp_hero, hp_enemy, potions = functions.turn_player(hp_hero, hp_enemy, potions)
     functions.show_state(hp_hero = max(0, hp_hero), hp_enemy = max(0, hp_enemy))
